610aea13c9c26bcddd357ee4e39ad32eb6591a76
Ko Hyunjoung <hyunjoung.ko@gmail.com>
2020-11-18 10:18:33 +0900

    a

---
+++ 201118/js/osulloc.js
@@ -0,0 +1,52 @@
+;(function(window,document,$,undefined){
+
+    var osulloc = {
+        init:   function(){
+            var that = this;
+
+                that.headerFn();
+                that.section01Fn();
+                that.section02Fn();
+                that.section03Fn();
+                that.section04Fn();
+                that.section05Fn();
+                that.section06Fn();
+                that.section07Fn();
+                that.section08Fn();
+                that.section09Fn();
+        },
+        headerFn:   function(){
+            
+        },
+        section01Fn:  function(){
+            
+        },
+        section02Fn:  function(){
+
+        },
+        section03Fn:  function(){
+
+        },
+        section04Fn:  function(){
+
+        },
+        section05Fn:  function(){
+
+        },
+        section06Fn:  function(){
+
+        },
+        section07Fn:  function(){
+
+        },
+        section08Fn:  function(){
+
+        },
+        section09Fn:  function(){
+
+        }
+    };
+
+    osulloc.init();
+
+})(window,document,jQuery);
